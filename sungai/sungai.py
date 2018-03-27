4e8c48ea2549ebc1c63d813a8e985dd364b46b68
habibo <thinkbibo@gmail.com>
2018-03-27 23:27:19 +0700

    Initial commit

---
+++ sungai/sungai.py
@@ -0,0 +1,26 @@
+#render1
+import mapnik
+m = mapnik.Map(812,566)
+m.background = mapnik.Color('lightblue')
+s = mapnik.Style()
+r = mapnik.Rule()
+polygon_symbolizer = mapnik.PolygonSymbolizer()
+polygon_symbolizer.fill = mapnik.Color('pink')
+r.symbols.append(polygon_symbolizer)
+
+line_symbolizer = mapnik.LineSymbolizer()
+line_symbolizer = mapnik.LineSymbolizer(mapnik.Color('#0fe3b9'), 2)
+line_symbolizer.stroke_width = 20.0
+
+#render1
+r.symbols.append(line_symbolizer)
+s.rules.append(r)
+m.append_style('My Style',s)
+ds = mapnik.Shapefile(file="SHP_Indonesia_sungai\IND_SNG_region.shp")
+layer = mapnik.Layer('m_habibi_04315004-1')
+layer.datasource = ds 
+layer.styles.append('My Style')
+m.layers.append(layer)
+m.zoom_all()
+mapnik.render_to_file(m,'m_habibi_04315004-1.png', 'png')
+print "rendered image to 'm_habibi_04315004-1.png'"
